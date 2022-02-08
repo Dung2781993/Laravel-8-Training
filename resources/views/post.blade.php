c3fc3b240ab52b3f1426804ecd9ad35e3495d429
Dung2781993@gmail.com <Dung2781993@gmail.com>
2022-02-08 19:15:39 +1100

    Store blog post as HTML files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,8 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My first post</a></h1>
-        <p>Miền Bắc đang trong chuỗi ngày mưa rét, ẩm ướt. Dự báo, đêm nay và ngày mai (9/2), khu vực này tiếp tục đón đợt không khí lạnh tăng cường, thời tiết tiếp tục rét đậm, rét hại. </p>
+        <?= $post; ?>
     </article>
-    <a href="/">Go back </a>
+    <a href="/">Go back</a>
 </body>
